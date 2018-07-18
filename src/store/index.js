ac73684dff945fb3a2264af6826aa0802715b272
oblivioussing <oblivioussing@outlook.com>
2018-07-18 22:46:24 +0800

    添加store

---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import Vue from 'vue'
+import Vuex from 'vuex'
+import menuTabs from './modules/menuTabs' //主菜单切换
+
+Vue.use(Vuex)
+
+export default new Vuex.Store({
+	modules: {
+		menuTabs
+	}
+})
