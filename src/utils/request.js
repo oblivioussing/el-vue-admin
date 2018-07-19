89f3903233c26bb5ada4adb839f36d16f43bd219
oblivioussing <oblivioussing@outlook.com>
2018-07-19 10:54:41 +0800

    request请求

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,78 +1,53 @@
-import router from '../router'
 import axios from 'axios'
 import qs from 'qs'
+import config from './config'
 
-let u = {}
-// base模块get请求
-u.getBase = async (path, params) => {
-  return await $request.get(CX_BASE_URL + path, params)
-}
-// base模块post请求
-u.postBase = async (path, params) => {
-  return await $request.post(CX_BASE_URL + path, params)
-}
-// mao模块get请求
-u.getMao = async (path, params) => {
-  return await $request.get(CX_MAO_URL + path, params)
-}
-// mao模块post请求
-u.postMao = async (path, params) => {
-  return await $request.post(CX_MAO_URL + path, params)
-}
-// get请求
-u.get = async (path, params) => {
-  const config = {
-    method: 'get',
-    url: path,
-    params: params
-  }
-  return await $request.request(config)
-}
-// post请求
-u.post = async (path, params) => {
-  const config = {
-    method: 'post',
-    url: path,
-    data: qs.stringify(params, { indices: false })
-  }
-  return await $request.request(config)
-}
-// 上传文件
-u.upFile = async (path, params) => {
-  const config = {
-    method: 'post',
-    headers: { 'Content-Type': 'multipart/form-data' },
-    url: path,
-    data: qs.stringify(params)
-  }
-  return await $request.request(config)
-}
-// 发起请求
-u.request = config => {
-  return new Promise(resolve => {
+// 接口请求地址
+const host = config.url
+
+const request = {
+  // get请求
+  async get(path, params) {
+    const config = {
+      method: 'get',
+      url: host + path,
+      params: params
+    }
+    return await request.request(config)
+  },
+  // post请求
+  async post(path, params) {
+    const config = {
+      method: 'post',
+      url: host + path,
+      data: qs.stringify(params, { indices: false })
+    }
+    return await request.request(config)
+  },
+  // 上传文件
+  async upFile(path, params) {
+    const config = {
+      method: 'post',
+      headers: { 'Content-Type': 'multipart/form-data' },
+      url: host + path,
+      data: qs.stringify(params)
+    }
+    return await request.request(config)
+  },
+  // 发起请求
+  request(config) {
     config.withCredentials = true
-    config.timeout = TIME_OUT
-    axios(config)
-      .then(ret => {
-        const resultCode = ret.data.resultCode
-        const stateArr = [
-          AUTH_FAIL,
-          NO_LOGIN,
-          LONGIN_EXPIRE,
-          TOKEN_SALT_NOT_NULL
-        ]
-        if (stateArr.indexOf(resultCode) >= 0) {
-          router.push({ path: '/login' })
-        } else if (resultCode === AUTH_FAIL) {
-          router.push({ path: '*' })
-        } else {
+    config.timeout = 10000
+    return new Promise(resolve => {
+      axios(config)
+        .then(ret => {
           resolve(ret.data)
-        }
-      })
-      .catch(err => {
-        resolve({ error: err, resultMsg: '服务器错误' })
-      })
-  })
+        })
+        .catch(err => {
+          resolve({ error: err, resultMsg: '服务器错误' })
+        })
+    })
+  }
 }
 
-export default u
+export default request
