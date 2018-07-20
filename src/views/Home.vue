9d95e40cc207d5bbd651c169117a1ffc67464c3c
oblivioussing <oblivioussing@outlook.com>
2018-07-21 01:11:31 +0800

    首页样式

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,43 +1,30 @@
 <template>
   <el-container class="h-100">
-    <el-aside>
-      <el-radio-group v-model="isCollapse" style="margin-bottom: 20px;">
-        <el-radio-button :label="false">展开</el-radio-button>
-        <el-radio-button :label="true">收起</el-radio-button>
-      </el-radio-group>
-      <el-menu default-active="1-4-1" class="el-menu-vertical-demo" :collapse="isCollapse">
-        <el-submenu index="1">
-          <template slot="title">
-            <i class="el-icon-location"></i>
-            <span slot="title">导航一</span>
-          </template>
-          <el-menu-item-group>
-            <span slot="title">分组一</span>
-            <el-menu-item index="1-1">选项1</el-menu-item>
-            <el-menu-item index="1-2">选项2</el-menu-item>
-          </el-menu-item-group>
-          <el-menu-item-group title="分组2">
-            <el-menu-item index="1-3">选项3</el-menu-item>
-          </el-menu-item-group>
-          <el-submenu index="1-4">
-            <span slot="title">选项4</span>
-            <el-menu-item index="1-4-1">选项1</el-menu-item>
-          </el-submenu>
-        </el-submenu>
-        <el-menu-item index="2">
-          <i class="el-icon-menu"></i>
-          <span slot="title">导航二</span>
-        </el-menu-item>
-        <el-menu-item index="3" disabled>
-          <i class="el-icon-document"></i>
-          <span slot="title">导航三</span>
-        </el-menu-item>
-        <el-menu-item index="4">
-          <i class="el-icon-setting"></i>
-          <span slot="title">导航四</span>
-        </el-menu-item>
-      </el-menu>
-    </el-aside>
+    <el-menu default-active="1-4-1" class="el-menu-vertical" :collapse="isCollapse">
+      <div class="el-menu-header">
+        <img class="gravity-center" src="../assets/img/logo_large.svg">
+      </div>
+      <el-submenu index="1">
+        <template slot="title">
+          <i class="el-icon-location"></i>
+          <span slot="title">导航一</span>
+        </template>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
+      </el-submenu>
+      <el-menu-item index="2">
+        <i class="el-icon-menu"></i>
+        <span slot="title">导航二</span>
+      </el-menu-item>
+      <el-menu-item index="3" disabled>
+        <i class="el-icon-document"></i>
+        <span slot="title">导航三</span>
+      </el-menu-item>
+      <el-menu-item index="4">
+        <i class="el-icon-setting"></i>
+        <span slot="title">导航四</span>
+      </el-menu-item>
+    </el-menu>
     <el-container>
       <el-header>Header</el-header>
       <el-main>Main</el-main>
@@ -50,17 +37,20 @@ export default {
   name: 'home',
   data() {
     return {
-      isCollapse: true
+      isCollapse: false
     }
-  },
-  created() {},
-  methods: {}
+  }
 }
 </script>
 
-<style>
-.el-menu-vertical-demo:not(.el-menu--collapse) {
+<style lang="scss">
+.el-menu-vertical {
+  .el-menu-header {
+    height: 60px;
+    position: relative;
+  }
+}
+.el-menu-vertical:not(.el-menu--collapse) {
   width: 200px;
-  min-height: 400px;
 }
 </style>
