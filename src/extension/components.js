cc13199d04fee697b83ec0ea34854b31d646947e
oblivioussing <oblivioussing@outlook.com>
2019-03-06 17:18:04 +0800

    优化

--- src/extension/components.js
+++ src/extension/components.js
@@ -1,5 +1,20 @@
 import Vue from 'vue'
 
-import Pagination from '@/components/Pagination' // 分页
-
-Vue.component('pagination', Pagination)
+// 动态获取组件
+let system = require.context('../components/system', false, /\.vue$/) // 系统
+let business = require.context('../components/system', false, /\.vue$/) // 业务
+// 文件映射
+const map = { system, business }
+// 遍历组件
+for (let item in map) {
+  // 赋值为全局组件
+  vest(map[item])
+}
+// 赋值为全局组件
+function vest (contexts) {
+  contexts.keys().forEach(key => {
+    let component = contexts(key).default
+    const name = key.replace(/(\.\/|\.vue)/g, '')
+    Vue.component(name, component)
+  })
+}
