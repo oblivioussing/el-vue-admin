bdd131beaaa18b1445433ccf714d2836db32edfe
oblivioussing <oblivioussing@outlook.com>
2019-03-06 14:51:56 +0800

    添加gzip压缩

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,5 @@
 const webpack = require('webpack')
+const CompressionPlugin = require('compression-webpack-plugin')
 const environment = require('./build/environment')
 
 module.exports = {
@@ -9,6 +10,12 @@ module.exports = {
       new webpack.DefinePlugin({
         'process.env.STAGE': JSON.stringify(environment.stage),
         'process.env.LOCAL_URL': JSON.stringify(environment.localUrl)
+      }),
+      // gzip压缩
+      new CompressionPlugin({
+        test: /\.js$|\.html$|\.css/, // 匹配文件名
+        threshold: 10240, // 超过10k进行压缩
+        deleteOriginalAssets: false // 是否删除源文件
       })
     ],
     // 使用cdn
