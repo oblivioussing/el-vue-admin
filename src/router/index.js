59a953a7f8adce4d05bfe168e2b70704c03a920c
oblivioussing <oblivioussing@outlook.com>
2019-05-05 17:09:41 +0800

    router优化

--- src/router/index.js
+++ src/router/index.js
@@ -1,17 +1,18 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 
-Vue.use(Router)
+import system from './modules/system' // 系统
+import order from './modules/order' // 订单
+import setting from './modules/setting' // 设置
 
-// 动态获取模块文件
-const files = require.context('./modules', true, /\.js$/)
-let routes = []
-files.keys().forEach(key => {
-  routes = routes.concat(files(key).default)
-})
+Vue.use(Router)
 
 export default new Router({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: [
+    ...system, // 系统
+    order, // 订单
+    setting // 设置
+  ]
 })
