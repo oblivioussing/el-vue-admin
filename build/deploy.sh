9a8af3e6cf2dd769c8ee5da7b9caa1f9b599a22a
oblivioussing <oblivioussing@outlook.com>
2019-05-09 11:00:39 +0800

    shell脚本优化

--- build/deploy.sh
+++ build/deploy.sh
@@ -1,3 +1,12 @@
 #! /bin/bash
 
+current_path = "../dist/*"
+target_path = "/srv/www/admin/"
+# 打包项目
 npm run build
+# 删除原文件
+rm -rf $target_path
+# 创建目录
+mkdir $target_path
+# 复制文件
+cp -ri $current_path $target_path
