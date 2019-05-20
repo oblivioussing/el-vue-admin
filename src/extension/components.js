fbdd83e9af3c8dd9b26215d5f27d6f4fa73b9232
oblivioussing <oblivioussing@outlook.com>
2019-05-20 11:21:50 +0800

    全局业务组件

--- src/extension/components.js
+++ src/extension/components.js
@@ -1,10 +1,20 @@
 import Vue from 'vue'
 
 // 动态获取组件
-let files = require.context('../components/global', false, /\.vue$/)
-
-files.keys().forEach(key => {
-  let component = files(key).default
-  const name = key.replace(/(\.\/|\.vue)/g, '')
-  Vue.component(name, component)
-})
+let system = require.context('../components/system', false, /\.vue$/) // 系统
+let business = require.context('../components/business', false, /\.vue$/) // 业务
+// 文件映射
+const map = { system, business }
+// 遍历组件
+for (let item in map) {
+  // 赋值为全局组件
+  vest(map[item])
+}
+// 赋值为全局组件
+function vest (contexts) {
+  contexts.keys().forEach(key => {
+    let component = contexts(key).default
+    const name = key.replace(/(\.\/|\.vue)/g, '')
+    Vue.component(name, component)
+  })
+}
