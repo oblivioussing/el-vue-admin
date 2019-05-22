02ead792707c19cb6d3f258c08604c7238ba8c9a
oblivioussing <oblivioussing@outlook.com>
2019-05-22 14:15:51 +0800

    文件夹名称修改

--- src/extension/components.js
+++ src/extension/components.js
@@ -1,10 +1,10 @@
 import Vue from 'vue'
 
 // 动态获取组件
-let global = require.context('../components/global', false, /\.vue$/) // 系统
+let system = require.context('../components/system', false, /\.vue$/) // 系统
 let business = require.context('../components/business', false, /\.vue$/) // 业务
 // 文件映射
-const map = { global, business }
+const map = { system, business }
 // 遍历组件
 for (let item in map) {
   // 赋值为全局组件
